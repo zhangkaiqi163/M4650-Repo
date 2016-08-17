c956713268f7a0d5091463a032efcd8405f06762
Varis Carey <Undertrick@variss-mbp.ucdenver.pvt>
2016-08-17 10:59:22 -0600

    added original course hello

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+
+
+# set up the Hello World string
+str="Hello M4650,"
+#In python you can concatentate strings using the addition(+) operator
+print(str+" my name is Varis Carey")
+#Add your hello statment below this line of code
