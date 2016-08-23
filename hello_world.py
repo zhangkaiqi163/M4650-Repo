66e8f451d163752aa2ebdb0cd24234629da1f1f0
bentleya <andrew.bentley@ucdenver.edu>
2016-08-23 11:55:26 -0600

    Andrew Bentley's submission for the Basics of Git homework

--- hello_world.py
+++ hello_world.py
@@ -5,3 +5,4 @@ str="Hello M4650,"
 #In python you can concatentate strings using the addition(+) operator
 print(str+" my name is Varis Carey")
 #Add your hello statment below this line of code
+print(str+" my name is Andrew Bentley")
